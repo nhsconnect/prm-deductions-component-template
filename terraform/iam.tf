735b7a82586136c2913388b120cf87b48447eefa
Phil Evans <phil.evans@thoughtworks.com>
2020-02-07 14:13:39 +0000

    Add IAM permissions for SSM to ECS Task Role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -16,3 +16,25 @@ resource "aws_iam_role" "component-ecs-role" {
   assume_role_policy = data.aws_iam_policy_document.ecs-assume-role-policy.json
   description        = "Role assumed by ${var.component_name} ECS task"
 }
+
+data "aws_iam_policy_document" "ssm_policy_doc" {
+  statement {
+    actions = [
+      "secretsmanager:*",
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ssm_policy" {
+  name   = "${var.environment}-{$var.component_name}-ssm"
+  policy = "${data.aws_iam_policy_document.ssm_policy_doc.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_police_attach" {
+  role       = "${aws_iam_role.component-ecs-role.name}"
+  policy_arn = aws_iam_policy.ssm_policy.arn
+}
