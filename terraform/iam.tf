92bf311e00bef3861b1afe1c07db54379042be0e
Phil Evans <phil.evans@thoughtworks.com>
2020-02-07 16:44:33 +0000

    Add IAM permissions for SSM to ECS Task Role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -33,6 +33,18 @@ data "aws_iam_policy_document" "ecr_policy_doc" {
   }
 }
 
+data "aws_iam_policy_document" "logs_policy_doc" {
+  statement {
+    actions = [
+      "logs:*"
+    ]
+
+    resources = [
+            "*"
+    ]
+  }
+}
+
 data "aws_iam_policy_document" "ssm_policy_doc" {
   statement {
     actions = [
@@ -55,6 +67,11 @@ resource "aws_iam_policy" "ecr_policy" {
   policy = data.aws_iam_policy_document.ecr_policy_doc.json
 }
 
+resource "aws_iam_policy" "logs_policy" {
+  name   = "${var.environment}-${var.component_name}-logs"
+  policy = data.aws_iam_policy_document.logs_policy_doc.json
+}
+
 resource "aws_iam_role_policy_attachment" "ssm_policy_attach" {
   role       = aws_iam_role.component-ecs-role.name
   policy_arn = aws_iam_policy.ssm_policy.arn
@@ -64,3 +81,9 @@ resource "aws_iam_role_policy_attachment" "ecr_policy_attach" {
   role       = aws_iam_role.component-ecs-role.name
   policy_arn = aws_iam_policy.ecr_policy.arn
 }
+
+resource "aws_iam_role_policy_attachment" "logs_policy_attach" {
+  role       = aws_iam_role.component-ecs-role.name
+  policy_arn = aws_iam_policy.logs_policy.arn
+}
+
