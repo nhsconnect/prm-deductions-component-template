8c21e15609aa6b6962ab34cca3da5d66e692df95
Helen Zhou <helen.zhou@thoughtworks.com>
2020-03-12 11:47:25 +0000

    [PRMT-1041] Will only send canary update message if updates are available

--- scripts/send-canary-update.js
+++ scripts/send-canary-update.js
@@ -3,37 +3,41 @@
 const axios = require('axios');
 const { getAllUpdatesText } = require('./parse-dependency-updates');
 
-const options = {
-  headers: {
-    'Content-Type': 'application/json'
-  }
-};
+const message = getAllUpdatesText(process.argv);
 
-const body = {
-  cards: [
-    {
-      header: {
-        title: 'component-template',
-        subtitle: 'Canary',
-        imageUrl:
-          'https://imageog.flaticon.com/icons/png/512/185/185862.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF'
-      },
-      sections: [
-        {
-          widgets: [
-            {
-              keyValue: {
-                topLabel: 'Updates Available',
-                content: getAllUpdatesText(process.argv)
+if (message) {
+  const body = {
+    cards: [
+      {
+        header: {
+          title: 'component-template',
+          subtitle: 'Canary',
+          imageUrl:
+            'https://imageog.flaticon.com/icons/png/512/185/185862.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF'
+        },
+        sections: [
+          {
+            widgets: [
+              {
+                keyValue: {
+                  topLabel: 'Updates Available',
+                  content: message
+                }
               }
-            }
-          ]
-        }
-      ]
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  const options = {
+    headers: {
+      'Content-Type': 'application/json'
     }
-  ]
-};
+  };
 
-console.log('Sending message to Google Hangout group');
+  console.log('Sending message to Google Hangout group');
 
-axios.post(process.env.GOOGLE_CHAT_CANARY_WEBHOOK, body, options);
+  axios.post(process.env.GOOGLE_CHAT_CANARY_WEBHOOK, body, options);
+}
