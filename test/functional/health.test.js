65f51c6268a37c8f39b8569812b0ec340acab568
Helen Zhou <helen.zhou@thoughtworks.com>
2020-03-13 14:20:48 +0000

    Rename tests to include type of request

--- test/functional/health.test.js
+++ test/functional/health.test.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 import adapter from 'axios/lib/adapters/http';
 
-describe('/health', () => {
-  it('health endpoint returns matching data', async done => {
-    const res = await axios.get(`${process.env.SERVICE_URL}/health`, {adapter});
+describe('GET /health', () => {
+  it('should return matching data', async done => {
+    const res = await axios.get(`${process.env.SERVICE_URL}/health`, { adapter });
 
-    expect(res.data).toEqual(expect.objectContaining({
-      version: '1.0.1',
-      description: 'Health of Deduction PRM Component Template',
-      status: 'running'
-    }));
+    expect(res.data).toEqual(
+      expect.objectContaining({
+        version: '1.0.1',
+        description: 'Health of Deduction PRM Component Template',
+        status: 'running'
+      })
+    );
     done();
   });
 });
