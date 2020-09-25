870a315afecea0842bf5e1b0a0f7d4ca45f18049
Rosie Amphlett <rosie.amphlett@thoughtworks.com>
2020-09-25 15:19:43 +0100

    [PRM-1130] Added key to backend bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,7 @@
 terraform{
       backend "s3" {
         bucket = "prm-deductions-terraform-state"
+        key    = "component-template/terraform.tfstate"
         region = "eu-west-2"
         encrypt = true
     }
